22fc8f2510d915724b34c020d5bb6a726d30a0bb
Niedja92 <niedleticia@hotmail.com>
2024-06-06 22:13:43 -0300

    Atualização configurações do Backend

--- Backend/app.js
+++ Backend/app.js
@@ -21,6 +21,44 @@ const db = mysql2.createConnection({
     database: process.env.database
 });
 
+app.get("/clientes", (req, res) => {
+    db.query("SELECT cpf FROM info_projeto;", (err, results) => {
+        if (err) {
+            console.error("Erro na consulta ao Banco de dados:", err);
+        };
+        return res
+        .status (200)
+        .json(results);
+    });
+});
+
+app.post("/form", upload.single('imagem'), (req, res) => {
+    const projects =
+      "INSERT INTO info_projeto (`rg`,`cpf`,`estado`,`cidade`,`departamento`,`areaDatuacao`,`cargo`,`estadoProjeto`,`cidadeProjeto`,`nomeProjeto`,`email`,`senha`,`nivelEscolaridade`,`empregabilidade`, `imagem`) VALUES (?)";
+    const values = [
+      req.body.rg,
+      req.body.cpf,
+      req.body.estado,
+      req.body.cidade,
+      req.body.departamento,
+      req.body.areaDatuacao,
+      req.body.cargo,
+      req.body.estadoProjeto,
+      req.body.cidadeProjeto,
+      req.body.nomeProjeto,
+      req.body.email,
+      req.body.senha,
+      req.body.nivelEscolaridade,
+      req.body.empregabilidade,
+      req.file.path || null
+    ];
+  
+    db.query(projects, [values], (err, data) => {
+      if (err) return res.json(err);
+      return res.json("Projeto Cadastrado");
+    });
+  });
+
 
 app.listen(port, () => {
     console.log(`Conected to ${port}`);
--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,6 @@
-
 import { BrowserRouter } from "react-router-dom";
 import RoutesApp from "./routes";
-import "./App.css"
-
+import "./App.css";
 
 function App() {
   return (
