9180c5408d3c1e2f7e59a52e66aec3cf9953e8cc
Niedja92 <niedleticia@hotmail.com>
2024-06-09 00:14:46 -0300

    Acrescentando estrutura ao Sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,7 +1,59 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import { FaChartLine, FaClock, FaMap, FaUser } from "react-icons/fa";
+import {
+  FaArrowRightFromBracket,
+  FaMessage,
+  FaNoteSticky,
+  FaPhoneVolume,
+  FaTrash,
+} from "react-icons/fa6";
 
 const Sidebar = () => {
-  return <div>SIDEBAR</div>;
+  return (
+    <div>
+      <nav>
+        <ul>
+          <li>
+            <FaUser />
+            <span>PERFIL</span>
+          </li>
+          <li>
+            <FaMap />
+            <span>MAPA DE CALOR</span>
+          </li>
+          <li>
+            <FaChartLine />
+            <span>ESTATÍSTICA</span>
+          </li>
+          <li>
+            <FaNoteSticky />
+            <span>RELATÓRIO</span>
+          </li>
+          <li>
+            <FaClock />
+            <span>HISTÓRICO</span>
+          </li>
+          <li>
+            <FaMessage />
+            <span>FAQ</span>
+          </li>
+          <li>
+            <FaPhoneVolume />
+            <span>CONTATO</span>
+          </li>
+          <li>
+            <FaTrash />
+            <span>EXCLUIR</span>
+          </li>
+          <li>
+            <FaArrowRightFromBracket />
+            <Link to="/">SAIR</Link>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  );
 };
 
 export default Sidebar;
