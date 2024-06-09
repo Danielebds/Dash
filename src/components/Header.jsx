e26b9520778cda76859ea660a047e901f725bcdd
Niedja92 <niedleticia@hotmail.com>
2024-06-09 00:13:45 -0300

    Estruturando o componente Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,18 @@
 import React from "react";
 
-
 const Header = () => {
-
-  return <div className="bg-red-500">HEADER</div>;
-
-  
-
+  return (
+    <header className="bg-red-500">
+      <div>LOGO CATCH</div>
+      <div>
+        <span>CLIENTE: PREFEITURA ******</span>
+        <span>PROGRAMA: ******</span>
+        <span>RESPONSÁVEL: *******</span>
+        <span>QUANTITATIVO: ******</span>
+      </div>
+      <div>LOGO PREFEITURA</div>
+    </header>
+  );
 };
 
 export default Header;
